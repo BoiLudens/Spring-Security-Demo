b44afa655c8d05d5be1d5f3a6c25e9fdb85bfb63
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-08 12:50:12 -0500

    added basic helloworld for graphql

--- src/main/java/com/boiludens/demo/controller/HelloWorldController.java
+++ src/main/java/com/boiludens/demo/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
 package com.boiludens.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String helloWorld() {
+    @QueryMapping
+    public String hello() {
         return "potato";
     }
 }
