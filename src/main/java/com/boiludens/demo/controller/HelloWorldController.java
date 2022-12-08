2776870ad076d724300756e9b7ec1b07410cd166
Matthew Anthony Coker <mattacoker@gmail.com>
2022-12-08 13:26:51 -0500

    per method security enabled

--- src/main/java/com/boiludens/demo/controller/HelloWorldController.java
+++ src/main/java/com/boiludens/demo/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.boiludens.demo.controller;
 
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class HelloWorldController {
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @QueryMapping
-    public String hello() {
+    public String hello(Principal principal) {
+        System.out.println(principal);
         return "potato";
     }
+
 }
